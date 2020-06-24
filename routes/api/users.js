c5704aaed4e22ba9cdc9749a718500bcca2bf9f3
Cassie Triche <cassietriche@comcast.net>
2020-06-24 14:38:59 -0500

    Retrieve a single user by _id

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,4 +11,15 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/5ef399a2d3bbe44f6237bbfd', function(req,res){
+  
+    var userId = req.params.userId;
+     Users.findOne({'_id':userId}, function(err, user){
+       if(err){
+        return res.json({'success':false, 'error': err});
+      }
+       return res.json({'success':true, 'user': user});
+     });
+   });
+
 module.exports = router;
