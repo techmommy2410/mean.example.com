d3373de8a14756970b47eee25c2bbf5b87e352d7
Cassie Triche <cassietriche@comcast.net>
2020-06-24 15:09:27 -0500

    Auto-update the modified date on edit

--- models/users.js
+++ models/users.js
@@ -30,7 +30,13 @@ modified: {
   }
 });
 
+
 //Add unique validation properties to the model
 Users.plugin(uniqueValidator);
+
+Users.pre('save', function(next){
+    this.modified = new Date().toISOString();
+    next();
+  });
 
 module.exports  = mongoose.model('Users', Users);
